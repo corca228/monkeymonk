06bbd63c4db1385866a90ce21ebccfee6b727a53
corca228 <103827489+corca228@users.noreply.github.com>
2024-05-12 12:28:10 +0300

    Update file.js

--- file.js
+++ file.js
@@ -1,5 +1,6 @@
+
 let text1 = "Быстро и ловко печатаю на клавиатуре, улучшая свои навыки каждый день. С каждой минутой я становлюсь все более уверенным в своей печатной скорости и точности. Клавиатура не является для меня барьером, а инструментом для самовыражения и удовольствия."
-let text2 = "Печать – это навык, который требует постоянной практики и усердной работы. Я постоянно улучшаю свою печатную скорость и точность, потому что я знаю, что он будет полезен во многих аспектах моей жизни. Будущее мое – за монитором и клавиатурой!"
+let text2 = "Печать это навык, который требует постоянной практики и усердной работы. Я постоянно улучшаю свою печатную скорость и точность, потому что я знаю, что он будет полезен во многих аспектах моей жизни."
 let text3 = "Я улучшаю свои навыки печати, так как понимаю, что это умение будет полезным в мире компьютеров и интернета. Я нацелен на постоянное совершенствование, поэтому каждый день провожу тренировки. Моя печать становится более быстрой и точной с каждым днем."
 let text_eng1 = "I am improving my typing skills every day, aiming to type faster and with greater accuracy. Each keystroke brings me closer to my goal of effortless, efficient typing. With each practice session, I feel more confident and capable at the keyboard. Typing is not just a skill, but a gateway to productivity and self-expression."
 let text_eng2 = "My fingers fly across the keyboard as I dedicate myself to mastering the art of typing. I recognize that speed and precision are crucial in today's digital age. With each passing day, I am honing my typing abilities, ensuring that I can effortlessly and accurately communicate via the keyboard. Typing is not just a task, but an essential skill in our modern world."
@@ -11,7 +12,6 @@ let split_text3 = text3.split("")
 let split_text_eng1 = text_eng1.split("")
 let split_text_eng2 = text_eng2.split("")
 let split_text_eng3 = text_eng3.split("")
-    // example.textContent = text1
 
 let heading = document.querySelector('.printed')
 let lang = document.querySelector('.bar')
@@ -19,42 +19,59 @@ let eng = document.getElementById('eng')
 let rus = document.getElementById('rus')
 let main = document.querySelector('.main')
 let example = document.querySelector('.example')
+let windows= document.querySelector('.body')
 
 let screen_text = []
 
-function getRandomInt() {
-    return Math.floor(Math.random());
-}
-const russian_text = ['split_text1', 'split_text2', 'split_text3']
-const english_text = ['split_text_eng1', 'split_text_eng2', 'split_text_eng3']
-const rondom = getRandomInt(3)
+windows.addEventListener('keydown', function(e) {
+  if(e.key === ' ') {
+      e.preventDefault(); // Предотвращаем действие по умолчанию (прокрутку страницы)
+  }
+});
+
+const russian_text = [split_text1, split_text2, split_text3]
+const english_text = [split_text_eng1, split_text_eng2, split_text_eng3]
+// const rondom = getRandomInt(3)
+const rondom = Math.floor(Math.random() * 3)
 const result = russian_text[rondom]
 const result_eng = english_text[rondom]
-console.log(result)
 
 
-for (let index = 0; index < split_text1.length; index++) {
-    const element = `<span>${split_text1[index]}</span>`
+
+for (let index = 0; index < result.length; index++) {
+    const element = `<span>${result[index]}</span>`
     screen_text.push(element)
 }
 
 
 let i = 0
 let language = 'russ'
-let number = split_text1[i]
+// let number = split_text1[i]
+number=result[i]
+console.log(number)
 lang.addEventListener('click', function() {
     if (language == 'russ') {
         language = 'eng'
         eng.style.color = 'red'
         rus.style.color = 'black'
-        number = result_eng
-        console.log(number)
+        screen_text=[]
+        for (let index = 0; index < result_eng.length; index++) {
+          const element = `<span>${result_eng[index]}</span>`
+          screen_text.push(element)}
+        heading.innerHTML = screen_text.join('')
+        heading.style.color = 'gray'
+        number=result_eng[i]
     } else {
         language = 'russ'
         rus.style.color = 'red'
         eng.style.color = 'black'
-        number = result
-        console.log(number)
+        screen_text=[]
+        for (let index = 0; index < result.length; index++) {
+          const element = `<span>${result[index]}</span>`
+          screen_text.push(element)}
+        heading.innerHTML = screen_text.join('')
+        heading.style.color = 'gray'
+        number=result[i]
     }
 })
 
@@ -77,9 +94,14 @@ function trackKeyboardActivity() {
         if (number == keyPressed) {
             children[i].style.color = 'white'
             i = i + 1
-            number = split_text1[i]
+            if (number==result_eng[i]){
+              number=result_eng[i]
+            }else{
+              number=result[i]}
+            
             children[i - 1].classList.remove('current') // удаление каретки для предыдущего символа
             children[i].classList.add('current')
+            console.log(result)
         } else {
             children[i].style.color = 'red'
             console.log('ERROR!' + number)
